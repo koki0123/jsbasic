09b93224830f38b72020db6c0089a88ac9d0ee40
西村公希 <>
2019-08-27 11:17:59 +0900

    console.logと四則演算子
    console.logの使い方と、+-*/%の使い方

---
+++ app.js
@@ -0,0 +1,14 @@
+
+console.log('----------------console.log----------------');
+
+console.log('hoge');
+
+console.log('----------------四則演算子----------------');
+
+console.log('1 + 1');
+console.log('1 - 7 - 5');
+console.log(1 * 3);
+console.log('1 / 3')
+console.log('5 % 3')
+
+console.log('5 - 2')
