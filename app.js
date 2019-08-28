f360e898a95c398972ac39cf1a6f4cd4431b43d3
西村公希 <>
2019-08-28 12:55:29 +0900

    28の授業分 関数、タイマー処理、配列途中まで練習

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ console.log('else if-----------');
 int = 5 ;
 if (int >=10) {
   console.jog(int + `は１０以上です`)
-}　else if (int > 3) {
+} else if (int > 3) {
   console.log(`${int}は３より大きいです。`);
 }
 else {
@@ -102,10 +102,11 @@ switch(signal) {
     console.log('止まれ');
     break;
   case 'green':
-    console.log('進め')
+    console.log('進め');
+    break;
   default:
-    console.log('その他')
-    break
+    console.log('その他');
+    break;
 }
 
 // 分岐が多いときはswitch,少ないときはif
@@ -120,3 +121,168 @@ if (number % 3 == 0) {
 }else if(number !== 3) {
   console.log(let)
 }
+
+console.log('----------------繰り返し処理----------------');
+
+// for(変数の定義、条件式、変数の更新)
+
+for (let i =1; i<= 10; i++) {
+  console.log(i);
+}
+
+console.log('while-----------')
+let j = 11;
+while (j <= 20) {
+  console.log(j);
+  j+= 2;
+}
+
+console.log('-------------アラート------------');
+// alert('こんにちは');
+// const answer = confirm('削除しますか?');
+// console.log(answer);
+
+// const value = prompt('あなたの年齢はいくつですか？', '不明');
+// console.log('${value}です');
+
+// 100~0までconsole.logで出力
+// 繰り返し処理して３の倍数でBuzz
+//if,elseを使って５の倍数でFizz
+// 5と3の倍数の時にFizz Buzzと出力する
+
+// for (let i = 100; i >= 0; i--) {
+//   console.log(i);
+//   if (i % 3 == 0 && i % 5 == 0){
+//     console.log('Fizz Buzz')
+//   }
+//   else if (i % 3 == 0) {
+//     console.log('Buzz')
+//   } else if(i % 5 == 0){
+//     console.log('Fizz')}
+//     else{
+//     console.log(i)
+//   }
+// }
+
+console.log('-----------関数-----------');
+// 関数のなかで使える変数を引数と呼ぶ
+
+/*
+    関数
+    複数の処理を1つにまとめたもの
+    function 関数名() {
+        処理
+    }
+    組み込み関数
+    alert()
+*/
+
+// function greeting() {
+//   console.log('おはよう');
+//   console.log('こんにちは');
+// }
+// greeting();
+
+// function greeting (greeting) {
+//   console.log(greeting);
+// }
+
+// greeting('こんにちは')
+// greeting('こんばんは')
+
+// console.log('ローカル変数---------')
+
+// function greeting(name) {
+//       let morning = 'おはよう' + name;
+//       return morning;
+// }
+
+
+// let hoge = greeting('');
+// console.log(hoge)
+
+// console.log('関数を変数に代入----------------');
+
+// let greeting = function (name) {
+//       let greeting = 'おはよう' + name;
+//       return greeting;
+// }
+
+// let hoge = greeting('seedKun');
+// console.log(hoge);
+
+// function addtax(product, number, tax) {
+//   let result= product * number * tax
+//   return result
+
+// }
+// let result = addtax(500, 10, 1.1);
+// console.log(addtax(500, 3, 1.08))
+
+// 勝った数/全試合数=100%
+
+// function SH(all, won) {
+//   let winrate = won / all *100
+//   return winrate
+// }
+
+// let winrate = SH(119, 64);
+// console.log(SH(119, 64))
+
+// 2000時間
+
+// function days (goal, time){
+//   let result = 2000 / time /365
+//   return result
+// }
+
+// let result = days (2000,3);
+// console.log(days(2000,3))
+
+console.log('----------------タイマー処理----------------');
+
+// console.log('setInterval----------------');
+// let i = 100;
+
+// function counDown() {
+//   console.log(i--);
+// }
+
+// setInterval(function() {
+//       countDown();
+//   }, 1000)
+
+  // console.log('setTimeout----------------');
+  // let j2 = 100;
+  //   function countDown() {
+  //       console.log(j2--);
+  //   }
+
+  //   setTimeout(function() {
+  //       countDown();
+  //   }, 1000);
+  // console.log('setTimeOutで繰り返し + timerをとめる----------------');
+  // let k = 100;
+  //   function countDown() {
+  //       console.log(k--);
+  //       let timer = setTimeout(function() {
+  //           countDown();
+  //       }, 1000);
+
+  //       if (k < 95) {
+  //           clearTimeout(timer);
+  //           console.log('timer stop');
+  //       }
+  //   }
+  //   countDown();
+
+  console.log('----------------配列----------------');
+  let student_list = ['Yamada','Sato','Suzuki'];
+  console.log(student_list);
+  console.log(student_list[0] = 'Kobayashi');
+  // チェーンメソッド
+student_list.push('Takahashi');
+console.log(student_list);
+console.log(student_list)
+// NFとAPVの合計額（１年）
+// ホークスの選手の配列
