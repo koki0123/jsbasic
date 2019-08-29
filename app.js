df4d9917c2181fe698657b4db7ed023fbeea0e7f
西村公希 <>
2019-08-29 12:30:10 +0900

    dateまで学習
    数値、日付、おみくじの配列

--- app.js
+++ app.js
@@ -276,13 +276,145 @@ console.log('----------------タイマー処理----------------');
   //   }
   //   countDown();
 
-  console.log('----------------配列----------------');
-  let student_list = ['Yamada','Sato','Suzuki'];
-  console.log(student_list);
-  console.log(student_list[0] = 'Kobayashi');
-  // チェーンメソッド
-student_list.push('Takahashi');
-console.log(student_list);
-console.log(student_list)
-// NFとAPVの合計額（１年）
+//   console.log('----------------配列----------------');
+//   let student_list = ['Yamada','Sato','Suzuki'];
+//   console.log(student_list);
+//   console.log(student_list[0] = 'Kobayashi');
+//   // チェーンメソッド
+// student_list.push('Takahashi');
+// console.log(student_list);
+// console.log(student_list)
+// NFとAPTの合計額（１年）
 // ホークスの選手の配列
+// NetFlix月額８４０円、Amazon Prime500円
+
+
+function all (NF, APT) {
+  let result = (NF + APT * 12)
+  return result
+}
+
+let result = all (840, 500)
+console.log(all(840, 500));
+
+console.log('配列---------')
+let player_list = ['Kokubo','Matsuda','Uchikawa'];
+  console.log(player_list);
+  console.log(player_list[0] = 'Yanagita');
+  player_list.push('Senga');
+  console.log(player_list);
+
+
+  // 連想配列
+  console.log('-----------------連想配列-------------------');
+
+  // let player = {
+  //   1: '内川',
+    
+  //   from: '大分'
+  // };
+
+  // let user2 = {
+  //   food: 'UFO',
+  //   age:'20',
+  //   from:'沖縄' 
+  // };
+
+  console.log('----------------2次元配列----------------');
+// let users = [user, user2];
+// console.log(users);
+// console.log(users[0]);
+// console.log(users[0]['age']);
+// console.log(users[1].from);
+
+let player = {9:'Yanagita',3:'Matsuda',1:'Uchikawa'};
+console.log(player[1]);
+
+// let player = {1:'Uchikawa'}
+let player2 = {1:'Suzuki'}
+
+let players = { softbank: player, carp: player2};
+console.log(players.carp[1]);
+
+// チーム名と背番号から選手名を出力する関数を作る
+// function showplayer (name, number) {
+// let result = players [team] [number];
+// return player;
+// }
+// console.log(showplayer(carp, 1));
+
+// console.log('------メソッド-------')
+// let user3 =
+//   from:'長崎',
+//    greet: function(name) {
+//     console.log("Hello I'm " + name + " from "+this.from);
+// }
+// }
+// user3.greet('Koki')
+
+console.log('組み込みオブジェクト---------');
+
+console.log('文字列-------');
+let str = "Koki";
+console.log(str.length);
+console.log(str.substr(3,5));
+
+console.log('---------配列');
+let arr = [1, 2, 3, 7, 8];
+console.log(arr, length);
+
+arr.push(4);
+console.log(arr);
+arr.splice(1, 0, 1.5);
+console.log(arr);
+// ４番目から１つ置き換える
+// 3.9
+arr.push(3);
+console.log(arr);
+arr.splice(3, 1 , 3.9);
+console.log(arr);
+
+console.log('数値----------');
+// 切り上げceil,切り捨てfloor, 四捨五入round,1未満の乱数
+console.log(Math.ceil(3.3));
+console.log(Math.floor(3.3));
+console.log(Math.round(3.4));
+console.log(Math.random(3.4));
+
+console.log(Math.random());
+console.log(Math.random()　* 10);
+
+// 配列
+// let day = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+// 乱数の結果を変数に入れる
+// おみくじの結果を表示
+// let day = Math.floor(Math.random() * 7);
+
+
+
+// console.log('日付----------------');
+// let date = new Date();
+// console.log(date);
+// console.log(date.getFullYear());
+// console.log(date.getMonth() + 1);
+// console.log(day[date.getDate()]);
+
+// 『今日は　年　月　日　曜日です』と出力
+// 各値をyear, month, date, day に一旦格別する
+let days = ['日', '月', '火', '水', '木', '金', '土'];
+
+let today = new Date();
+
+let year = today.getFullYear();
+
+let month = today.getMonth() + 1;
+
+let date = today.getDate();
+
+let day = days[today.getDay()];
+
+
+
+     console.log("今日は " + year + " 年 " + month + " 月 " + date + " 日 " + day + " 曜日です。 ");
+
+     
